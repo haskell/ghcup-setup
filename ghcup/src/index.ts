9ecf22130925c5236eebc56f724bb358284224d1
Andrea Bedini <andrea@andreabedini.com>
2024-12-12 11:16:00 +0800

    feat: use last GHCup version by default

--- ghcup/src/index.ts
+++ ghcup/src/index.ts
@@ -2,10 +2,10 @@ import { main } from "./main.ts"
 import core from "@actions/core";
 
 try {
-main({
-  version: core.getInput("version"),
-  extra_release_channels: core.getMultilineInput("extra-release-channels")
-})
+  main({
+    version: core.getInput("version"),
+    extra_release_channels: core.getMultilineInput("extra-release-channels")
+  })
 } catch (error) {
   core.setFailed((error as Error).message);
 }
