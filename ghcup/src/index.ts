cb2a74650483a31d0b59ae2401d7633421201745
Julian Ospald <hasufell@posteo.de>
2025-01-01 22:55:51 +0800

    Use getInputAsBool

--- ghcup/src/index.ts
+++ ghcup/src/index.ts
@@ -1,11 +1,11 @@
-import { main, parseYAMLBoolean } from "./main.ts"
+import { main, getInputAsBool } from "./main.ts"
 import core from "@actions/core";
 
 try {
   main({
     version: core.getInput("version"),
     release_channels: core.getMultilineInput("release-channels"),
-    stack_hook: parseYAMLBoolean("stack-hook", core.getInput("stack-hook")),
+    stack_hook: getInputAsBool("stack-hook"),
   })
 } catch (error) {
   core.setFailed((error as Error).message);
